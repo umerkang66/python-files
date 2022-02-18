79f1a2b1488d25b4f28004f04341cb918bab5aac
Umer Kang <umerkang66@gmail.com>
2022-02-18 09:38:38 +0500

    complete 20 videos of andrei neagioe course

--- python-course-andrei-neagoie/python-fundamentals/strings.py
+++ python-course-andrei-neagoie/python-fundamentals/strings.py
@@ -1,5 +1,6 @@
 name = "umer is my name"
 
+# by doing all of the operations, original string is not changed
 # get access from certain index to certain index, the last index will not be included
 print(name[2:8])
 
@@ -7,8 +8,29 @@ print(name[2:8])
 # "stepover" is how many items to stepover, the default is 1, because we are going one by one, but can change it
 print(name[0:8:2])
 
-# In python the negative index means start at the end of array or string
+# In python the negative index means start at the end of list or string
 
 # reverse a string
 # it means start from zero, end from the last element, stepover from the last element means reverse it
 print(name[::-1])
+
+# IMMUTABILITY
+# strings in python are immutable, means they cannot be changed
+immutable_str = "my name is umer"
+# immutable_str[0] = "y"
+
+# CALCULATING THE LENGTH OF THE STRING
+length_of_str = len(immutable_str)
+print(length_of_str)
+
+# STRING METHODS
+# upper() method doesn't change the original string
+print(name.upper())
+# capitalize the first element of the string
+print(name.capitalize())
+
+# returns the starting from where it starts
+print(name.find('is'))
+
+# this replaces all the occurrences of "is", and returns the new string, don't mutate the original string
+print(name.replace('is', 'are'))
