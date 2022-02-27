6cdffd8f14a201650508f82ad74e15a849892c5c
Umer Kang <umerkang66@gmail.com>
2022-02-27 21:56:51 +0500

    complete 11 videos of oop in andrei course

--- python-course-andrei-neagoie/oop/Player.py
+++ python-course-andrei-neagoie/oop/Player.py
@@ -1,10 +1,31 @@
 class PlayerCharacter:
     membership = True
 
-    def __init__(self, name, birthYear):
+    def __init__(self, name, age):
         if self.membership:
             self.name = name
-            self.birthYear = birthYear
+            self.age = age
 
     def get_age(self, currentYear):
-        return currentYear - self.birthYear
+        return currentYear - self.age
+
+    # we can use class state when we want to use class state
+    @classmethod
+    def adding_things(cls, num1, num2):
+        # cls is the current class
+        return cls("Umer", num1 + num2)
+
+    # we can use class state when we don't want to use class state
+    @staticmethod
+    # static method doesn't have access to the cls (current class)
+    def adding_things2(*nums):
+        sum = 0
+        for num in nums:
+            sum += num
+        return sum
+
+
+umer = PlayerCharacter.adding_things(12, 9)
+print(umer.age)
+
+print(PlayerCharacter.adding_things2(23, 12, 12, 32, 23))
